40ac7e765be228f5baee32a84d42f5e2d162bbe3
T333333-xtz <1505312983@qq.com>
2025-06-14 22:44:43 +0800

    Update UDPserver.py
    Get the size of the request file builds a response message with the file name, file size, and new port number to send the response to the client

--- UDPserver.py
+++ UDPserver.py
@@ -9,3 +9,8 @@ def handleFileTransmission(file_name, client_address):
     new_port = random.randint(50000, 51000)
     new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     new_socket.bind(('0.0.0.0', new_port))
+
+try:
+        file_size = os.path.getsize(file_name)
+        response = f'OK {file_name} SIZE {file_size} PORT {new_port}'
+        new_socket.sendto(response.encode(), client_address)
