315a66d904a5297e903dda8e7cb4bafec992a5ad
T333333-xtz <1505312983@qq.com>
2025-06-14 22:40:08 +0800

    Update UDPserver.py
    The handleFileTransmission function is used to handle transfer requests for individual files

--- UDPserver.py
+++ UDPserver.py
@@ -4,3 +4,8 @@ import base64
 import random
 import os
 
+def handleFileTransmission(file_name, client_address):
+    # 选择一个随机端口号
+    new_port = random.randint(50000, 51000)
+    new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    new_socket.bind(('0.0.0.0', new_port))
