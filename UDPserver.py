9ffc6946a56eeeaa279fd320f0e74d95f1df7694
T333333-xtz <1505312983@qq.com>
2025-06-14 22:45:24 +0800

    Update UDPserver.py
    Open the requested file for binary reads, enter a loop and wait for the client request

--- UDPserver.py
+++ UDPserver.py
@@ -14,3 +14,13 @@ try:
         file_size = os.path.getsize(file_name)
         response = f'OK {file_name} SIZE {file_size} PORT {new_port}'
         new_socket.sendto(response.encode(), client_address)
+ with open(file_name, 'rb') as file:
+            while True:
+                data, client = new_socket.recvfrom(1024)
+                client_request = data.decode().strip()
+                parts = client_request.split(" ")
+
+                if parts[0] == 'FILE' and parts[3] == 'CLOSE':
+                    close_response = f'FILE {file_name} CLOSE_OK'
+                    new_socket.sendto(close_response.encode(), client)
+                    break
